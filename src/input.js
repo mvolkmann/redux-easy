3a7c1f79945fa1a346886f4001dab821a3e8bd3e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-01-14 18:12:00 -0600

    fixed issues in input.js

--- src/input.js
+++ src/input.js
@@ -1,37 +1,26 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {dispatch, getPathValue} from 'redux-easy';
 import {string} from 'prop-types';
-
-let path;
+import React from 'react';
+import {dispatchSet, getPathValue} from 'redux-easy';
 
 class Input extends React.Component {
-  handleChange(event) {
-    const {value} = event.target;
-    dispatch('@setPath', {path, value});
-  }
+  handleChange = event => {
+    const {checked, value} = event.target;
+    const {path, type} = this.props;
+    dispatchSet(path, type === 'checkbox' ? checked : value);
+  };
 
   render() {
-    ({path} = this.props);
-    return (
-      <input
-        type={this.props.type}
-        onChange={this.handleChange}
-        value={this.props.value}
-      />
-    );
-    //TODO: Add this to input? {...this.props}
+    const {path, type} = this.props;
+    const value = getPathValue(path);
+    const propName = type === 'checkbox' ? 'checked' : 'value';
+    const inputProps = {...this.props, [propName]: value};
+    return <input {...inputProps} onChange={this.handleChange} />;
   }
 }
+
 Input.propTypes = {
   path: string.isRequired,
-  type: string.isRequired,
-  value: string.isRequired
+  type: string.isRequired
 };
 
-const mapState = () => {
-  const value = path ? getPathValue(path) : undefined;
-  return {value};
-};
-
-export default connect(mapState)(Input);
+export default Input;
