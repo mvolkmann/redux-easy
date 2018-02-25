ccae3e76a6e0da08205875abae7100e016204a71
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-02-25 16:16:58 -0600

    another attempt to fix issue with accessing watch function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-//export {default as Checkboxes} from './checkboxes';
+export {default as Checkboxes} from './checkboxes';
 export {default as Input} from './input';
-//export {default as RadioButtons} from './radio-buttons';
+export {default as RadioButtons} from './radio-buttons';
 export {default as Select} from './select';
 export {default as TextArea} from './textarea';
 export * from './redux-easy';
